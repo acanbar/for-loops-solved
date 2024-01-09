a216bd6697331743d0024e7eb031d6ec253347d8
Andres Canas-Baratta <andres.canas@yahoo.com>
2024-01-08 22:03:51 -0800

    ex 1, 2 and half of 3 resolved

--- exercises/e3.js
+++ exercises/e3.js
@@ -9,8 +9,14 @@
  * */
 
 export function getAverage(array) {
-  // Your code goes here...
+  
+  let sum = 0;
 
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  const average = sum / array.length;
+  return average;
 }
 
 
